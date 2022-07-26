72a23c51c81d8c686fbc5bb9259d0d30b21462ef
Angelurbina4 <angelurbina44@gmail.com>
2022-07-26 18:14:11 -0400

    environment variable

--- server/index.js
+++ server/index.js
@@ -18,10 +18,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes); //Cada ruta empezara con /posts
 
-const CONNECTION_URL = process.env.CONNECTION_URL;
+// const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
